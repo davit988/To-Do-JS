0c9f6abfbbf9d33803d13e352d17961ff659a30e
davit988 <archehovsepyan@gmail.com>
2026-09-17 17:22:42 +0400

    add delete button

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const taskList = document.querySelector("#taskList");
 
 add.addEventListener("click", addTask);
 
+let id = 0;
 function addTask() {
   const text = input.value.trim();
 
@@ -11,8 +12,31 @@ function addTask() {
     return;
   }
 
+  id++;
+
   const li = document.createElement("li");
+
   li.textContent = text;
+
+  li.id = id;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+
+  deleteBtn.id = id;
+
+  deleteBtn.addEventListener("click", () => {
+    const allTask = document.querySelectorAll("li");
+
+    allTask.forEach((task) => {
+      if (task.id === deleteBtn.id) {
+        task.remove();
+      }
+    });
+  });
+
+  li.append(deleteBtn);
+
   taskList.append(li);
   input.value = "";
 }
